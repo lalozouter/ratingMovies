8012c16ca63cee366f5936d454d1367ca98b9709
lalozouter <lalozouter@gmail.com>
2020-02-14 13:15:25 -0600

    initial commit

---
+++ discovery-server/src/test/java/com/latzche/microservice/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.latzche.microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
